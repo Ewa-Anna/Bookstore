31388b2baf73c99e6aab1e7b255731a58660b954
Ewa-Anna <ewaanna073@gmail.com>
2023-11-11 12:47:32 +0100

    Fixed bug #48 and added webhooks

--- bookstore/orders/admin.py
+++ bookstore/orders/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Order, OrderItem
+
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+order_payment.short_description = 'Stripe Payment'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -14,9 +25,10 @@ class OrderAdmin(admin.ModelAdmin):
         "first_name",
         "last_name",
         "email",
+        "paid",
+        order_payment,
         "created",
         "updated",
-        "paid",
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
