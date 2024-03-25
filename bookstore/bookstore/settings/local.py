c82aeb6f96304d7ba7477182e3f6f6ec8ceee90b
Ewa-Anna <ewaanna073@gmail.com>
2024-03-25 20:13:37 +0100

    BugFix#102

--- bookstore/bookstore/settings/local.py
+++ bookstore/bookstore/settings/local.py
@@ -8,3 +8,5 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),  # noqa: F405
     }
 }
+
+DATABASE_ENGINE = 'sqlite3'
--- bookstore/tests/conftest.py
+++ bookstore/tests/conftest.py
@@ -249,10 +249,10 @@ def test_expired_coupon():
 
 
 # For Redis testing
-@pytest.fixture(autouse=True)
-def use_dummy_cache_backend(settings):
-    settings.CACHES = {
-        "default": {
-            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
-        }
-    }
+# @pytest.fixture(autouse=True)
+# def use_dummy_cache_backend(settings):
+#     settings.CACHES = {
+#         "default": {
+#             "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+#         }
+#     }
